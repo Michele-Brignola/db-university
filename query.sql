5b388f9c97be0f22d13426fcbca91337527ef578
Michele Brignola <michele.brignola2@gmail.com>
2026-03-11 20:06:14 +0100

    query completed

--- query.sql
+++ query.sql
@@ -32,6 +32,32 @@ WHERE
   and courses.year = 1;
 
 /* 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21) */
+SELECT
+  *
+FROM
+  university.exams
+WHERE
+  HOUR (exams.hour) >= 14
+  and exams.date = "2020/06/20";
+
 /* 6. Selezionare tutti i corsi di laurea magistrale (38) */
+SELECT
+  *
+FROM
+  university.degrees
+WHERE
+  degrees.level = "magistrale";
+
 /* 7. Da quanti dipartimenti è composta l'università? (12) */
+SELECT
+  *
+FROM
+  university.departments;
+
 /* 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50) */
+SELECT
+  *
+FROM
+  university.teachers
+WHERE
+  teachers.phone IS NULL;
